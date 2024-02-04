0377d89dae6e81f1d45996d0c4ffae13936d4192
IdeaProjects <harshitchourasiya0808@gmail.com>
2024-02-04 22:49:41 +0530

    Number & Math

--- 5_number_and_math.js
+++ 5_number_and_math.js
@@ -1,28 +1,41 @@
- const score = 400
- console.log(score)
+//  const score = 400
+//  console.log(score)
 
- const balance = new Number(100)
- console.log(balance)
+//  const balance = new Number(100)
+//  console.log(balance)
 
- console.log(balance.toString())
- console.log(typeof(balance));
- console.log(balance.toString().length)
- console.log(balance.toFixed(2));
+//  console.log(balance.toString())
+//  console.log(typeof(balance));
+//  console.log(balance.toString().length)
+//  console.log(balance.toFixed(2));
 
- const otherNum = 225.6848
- console.log(otherNum.toPrecision(3));
+//  const otherNum = 225.6848
+//  console.log(otherNum.toPrecision(3));
 
- const otherNum1 = 225.3848
- console.log(otherNum1.toPrecision(3));
+//  const otherNum1 = 225.3848
+//  console.log(otherNum1.toPrecision(3));
 
- const otherNum2 = 8225.3848
- console.log(otherNum2.toPrecision(3));
+//  const otherNum2 = 8225.3848
+//  console.log(otherNum2.toPrecision(3));
 
- const Num1 = 1000000
- console.log(Num1.toLocaleString());
- console.log(Num1.toLocaleString('en-IN'));
+//  const Num1 = 1000000
+//  console.log(Num1.toLocaleString());
+//  console.log(Num1.toLocaleString('en-IN'));
 
  // *********************Math*****************
 
- 
+ console.log(Math);
+ console.log(Math.abs(-32));
+ console.log(Math.round(4.56))
+ console.log(Math.ceil(4.2))
+ console.log(Math.floor(4.2))
+ console.log(Math.min(4,2,6,9,3))
+ console.log(Math.max(4,1,3,9,5))
+ console.log(Math.floor(Math.random()*10000))
+
+ const min = 10;
+ const max = 20;
+
+ const random = (Math.floor(Math.random() * (max - min + 1)) + min);
+ console.log(random)
 
