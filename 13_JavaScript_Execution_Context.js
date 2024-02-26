6d7db1fae25513bfef2c501451bd7bbf8a134661
IdeaProjects <harshitchourasiya0808@gmail.com>
2024-02-27 01:25:47 +0530

    13 JavaScript Execution Context

---
+++ 13_JavaScript_Execution_Context.js
@@ -0,0 +1,13 @@
+// JavaScript Execution Context
+
+// In the run of the code  javaScript is use the two phase to execute the program 
+
+// {} => Global Execution Context (this)
+
+// jab bhi hum javaScript ko koi bhi code dete hai to sabse phele Global Execution context (GEC) banta hai. aur jha bhi GEC banega vah usse ek varible this ko refer kar diya jata hai
+ 
+// * Global Execution Context
+// * Function / Functional Execution Context
+// * Eval Execution Context
+
+// jab humare pass ek javaScript ka code hota hai to ye 2 phases merun hota hai isme first humare pass memory creation phase
