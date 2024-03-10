6efaf904b75bd589aca3ac95c25d5cc59b3810ed
IdeaProjects <harshitchourasiya0808@gmail.com>
2024-03-10 17:54:57 +0530

    Filters Compleated

--- 18_Filter_Map_and_Reduse.js
+++ 18_Filter_Map_and_Reduse.js
@@ -1,7 +1,52 @@
-const coding = ['js', 'ruby', 'java', 'python', 'cpp', 'css', 'html']
+// const coding = ['js', 'ruby', 'java', 'python', 'cpp', 'css', 'html']
 
-const value = coding.forEach((item) => {
-    console.log(item);
+// const value = coding.forEach((item) => {
+//     console.log(item);
+// })
+
+// console.log(value);
+
+//------------------Filters-------------------
+
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+
+// const newNums = myNums.filter((num) => num > 4)
+
+const newNums = myNums.filter((num) => {
+    return num > 4
 })
 
-console.log(value);
+console.log(newNums)
+
+const newNums1 = []
+
+myNums.forEach((num) => {
+    if (num > 4) {
+        newNums1.push(num)
+    }
+})
+console.log(newNums1)
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  const userBooks = books.filter((book) => book.genre === 'History')
+
+//    const userBooks1 = books.filter((book) => (book.publish >= 1995))
+
+const userBooks1 = books.filter((book) => {    return book.publish >= 1995 && book.genre === 'History'
+})
+
+//   console.log(userBooks)
+  console.log(userBooks1)
+
+//----------------Map-------------------------
